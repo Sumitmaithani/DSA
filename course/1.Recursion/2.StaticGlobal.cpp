45130ee80dd52cc5e0fcd9a3656369bf324c0fe2
Sumit Maithani <86047367+Sumitmaithani@users.noreply.github.com>
2023-04-29 12:52:41 +0530

    recursion intro

---
+++ course/1.Recursion/2.StaticGlobal.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int x = 0;
+int fun(int n)
+{
+    if (n > 0)
+    {
+        x++;
+        return fun(n - 1) + x;
+    }
+    return 0;
+}
+
+int main()
+{
+    int r;
+    r = fun(5);
+    cout << r << ' ';
+
+    r = fun(5);
+    cout << r << ' ';
+
+    return 0;
+}
